d532333746f3655b5c24f7dd4979f0dbdd8a0ff5
fabian2912 <56194394+fabian2912@users.noreply.github.com>
2023-02-16 13:53:07 +0000

    testing parts of webapp

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 'use strict';
+// Testing different parts of the webapp
+/*
+console.log(document.querySelector('.message').textContent); // selects the element, and on that element we read the textContent property
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23; */
 
-document.querySelector('.message');
+document.querySelector('')
