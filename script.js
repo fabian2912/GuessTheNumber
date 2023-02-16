1dbe3e1c5315b30350e60c3a95f33256fd3ba5cf
fabian2912 <56194394+fabian2912@users.noreply.github.com>
2023-02-16 15:32:13 +0000

    added the 'Again' button functionality

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let score = 20;
 // define a secret number for the game; multiply by 20 for 1-20 
 //       Math.trunc to remove decimals 
 //       +1 because decimals after 19 will default to 19 i.e. 19.9999 will truncate to 19 so t20 is impossible
-const secretNumber = Math.trunc(Math.random() * 20) + 1; 
-document.querySelector('.number').textContent = secretNumber; 
+let secretNumber = Math.trunc(Math.random() * 20) + 1; 
+console.log(secretNumber);
 
  // adds a 'click' Event Listener to the check button, with a corresponding EventHandler function
 document.querySelector('.check').addEventListener('click', function() {
@@ -18,6 +18,9 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent = "You didn't guess a number";
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "Correct!";
+        document.querySelector('.number').textContent = secretNumber; 
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = "Too high...";
@@ -37,5 +40,21 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
+
+});
+
+// implement Reset button functionality
+document.querySelector('.again').addEventListener('click', function() {
+    console.log("cliked the 'Again' button")
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    console.log(secretNumber);
+
+    document.querySelector('.message').textContent = 'start guessing...'
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = '';
 
 });
